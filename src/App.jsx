72f4a1a20c605f3e4a084287ba1319317a05df87
wood.wu <kilokingw@gmail.com>
2024-09-02 16:03:11 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -125,7 +125,7 @@ function App() {
             isMobile ? "h-screen px-6 -mt-3" : "p-6"
           } `}
         >
-          <div className={` ${isMobile ? "w-full" : "pt-[1%] w-[700px]"}  `}>
+          <div className={` ${isMobile ? "w-full" : "pt-[1%] w-[48%]"}  `}>
             {videoId && (
               <div className="w-full  aspect-video drop-shadow-xl rounded-lg overflow-hidden">
                 <ReactPlayer
